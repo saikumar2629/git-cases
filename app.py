b61023f95c5f2a041c68d40a500d0debe7e160fb
saikumar2629 <gollusaikumar2000@gmail.com>
2025-04-15 20:07:16 +0530

    modified different line

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ hello
 how are you
 commit divergences are there 
 when you run a git push before git pull
-we have to pull the repository before going to push 
+we have to pull the repository before going to push
+got it saikumar?
